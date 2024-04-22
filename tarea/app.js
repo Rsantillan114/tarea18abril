ae7680b63bd0ed0b92e8b2f38d5b34ef4a232205
Rsantillan114 <161532601+Rsantillan114@users.noreply.github.com>
2024-04-22 00:44:09 -0700

    Add files via upload

--- tarea/app.js
+++ tarea/app.js
@@ -1,14 +1,27 @@
 document.getElementById('send').addEventListener('click', async function(){
-    const pokemon= document.getElementById('name').value;
-    await getPokemon(pokemon);
-});
+    const pokemonName = document.getElementById('name').value;
+    await displayPokemon(pokemonName);
+  });
+  async function getPokemon(name) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    return response.json();
+  }
+  
+  async function displayPokemon(name) {
+    const response = await getPokemon(name);
 
-async function getPokemon(name){
-    const response= await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-    const datos= response.json(pokemones);
-    return datos;
+    const imagen = document.querySelector('#informacion-pokemon img');
+    imagen.src = response.sprites.front_default;
+
+    const nombre = document.getElementById('nombre');
+    nombre.textContent = "Nombre: " + response.name;
+
+    const id = document.getElementById('id');
+    id.textContent = "ID: " + response.id;
+
+    const tipo = document.getElementById('tipo');
+    tipo.textContent = "Tipo(s): ";
+    response.types.forEach((type) => {
+        tipo.textContent += `${type.type.name}, `;
+    });
 }
-
-getPokemon().then(datos => {
-    console.log(datos);
-})
